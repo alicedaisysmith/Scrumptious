87d7244fd677a7485f38f95f8ce99c04fccbb9d3
Alice Smith <alicedaisysmith1@@gmail.com>
2022-08-11 17:23:35 +0100

    shout out state stored

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,31 @@
 import { useState } from "react";
 import "./Modal.css";
 
-const teams = [{teamName: 'One Response', members: ['Holly', 'Josh','Steve', 'Mike']}, {teamName: 'OneED', members: ['Matthew', 'Tyler', 'Lloyd', 'Rafael']}]
-const behaviours = ['Teamwork', 'Creativity', 'Hard Working', 'Above and beyond']
+const teams = [
+  { teamName: "One Response", members: ["Holly", "Josh", "Steve", "Mike"] },
+  { teamName: "OneED", members: ["Matthew", "Tyler", "Lloyd", "Rafael"] },
+];
+const behaviours = ["teamwork", "hardworking", "creativity", "independance", "above and beyond"]
 
 const Modal = ({ onCancelClick }) => {
   // we have passed in a variable (or property) into the modal component they need {} to work - we will add an on click into the cancel button
-  
-const [selectedTeam, setSelectedTeam] = useState() 
-const getSelectedTeam = (event) => {
-    setSelectedTeam(event.target.value) 
-}
+  const [selectedBehaviour, setSelectedBehaviour] = useState();
+  const storeSelectedBehaviour = (event) => {
+    setSelectedBehaviour(event.target.value);
+  };
+
+  const [selectedName, setSelectedName] = useState();
+  const storeSelectedName = (event) => {
+    setSelectedName(event.target.value);
+  };
+  const [selectedTeam, setSelectedTeam] = useState();
+  const storeSelectedTeam = (event) => {
+    setSelectedTeam(event.target.value);
+  };
+  const submitForm = () => {
+    console.log (`Congratulations to ${selectedName} from the ${selectedTeam} team for their ${selectedBehaviour}.`)
+  }
+
   return (
     <div className="modal">
       <div className="modalContent">
@@ -19,27 +34,45 @@ const getSelectedTeam = (event) => {
         </header>
         <div className="dropdowns">
           <label>Team name</label>
-          <select name="selectTeam" onChange={getSelectedTeam}>
-            <option selected value={'default'} disabled>Please select...</option>
-            {teams.map((team) => <option value= {team.teamName}>{team.teamName}</option>)}
-            
-         
+          <select name="selectTeam" onChange={storeSelectedTeam}>
+            <option selected value={"default"} disabled>
+              Please select...
+            </option>
+            {teams.map((team) => (
+              <option value={team.teamName}>{team.teamName}</option>
+            ))}
           </select>
           <label>Employee name</label>
-          <select name="selectTeam">
-          <option selected value={'default'} disabled>Please select...</option>
-            <option>Honky</option>
-            <option>Stonky</option>
-            <option>Bonky</option>
+          <select name="selectTeam" onChange={storeSelectedName}>
+            <option selected value={"default"} disabled>
+              Please select...
+            </option>
+            {teams[0].members.map((member) => (
+              <option value={member}>{member}</option>
+
+            ))}
+            
+            {/* <option value={"Honky"}>Honky</option> */}
+            {/* <option value={"Stonky"}>Stonky</option> */}
+            {/* <option value={"Bonky"}>Bonky</option> */}
           </select>
           <label>Shout out type</label>
-          <select name="selectTeam">
-            <option selected value={'default'} disabled>Please select...</option>
-            {behaviours.map((behaviour) => <option value={behaviour}>{behaviour}</option>)}
+          <select name="selectBehaviour" onChange={storeSelectedBehaviour}>
+            <option selected value={"default"} disabled>
+              Please select...
+            </option>
+            {behaviours.map((behaviour) => (
+              <option value={behaviour}>{behaviour}</option>
+            ))}
+            {/* <option value={"teamwork"}>Teamwork</option> */}
+            {/* <option value={"creativty"}>Creativity</option> */}
+            {/* <option value={"independance"}>Independance</option> */}
+            {/* <option value={"above and beyond"}>Above and Beyond</option> */}
+            {/* the commented out bits are what I initially wrote but then refactored it into maps to avoid having to write option again and again. The map itterates through the array and applies the same logic to each, in this case giving a value the same as the behaviour */}
           </select>
         </div>
         <p>modal body section</p>
-        <button>Submit shoutout</button>
+        <button onClick={submitForm}>Submit shoutout</button>
         <button onClick={onCancelClick}>Cancel shoutout</button>
       </div>
     </div>
--- src/components/ShoutOuts.js
+++ src/components/ShoutOuts.js
@@ -2,8 +2,18 @@ import "./ShoutOuts.css";
 import { useState } from "react";
 import Modal from "./Modal";
 
+
+//While we build the UI we want a list of pretend shout outs to help us see how it will look
+//Later we will move this list into a useState so that users can add their own shoutouts using the form but don't worry about this for now
+
+//Each shoutout is an string
+const shoutOuts = []
+
 const ShoutOuts = () => {
   const [showModal, setShowModal] = useState(false);
+  const displayShowModal = () => {
+    setShowModal(true)
+  }
   const falseShowModal = () => {
     setShowModal(false);
   };
@@ -11,10 +21,17 @@ const ShoutOuts = () => {
   return (
     <div>
       <h1>Give your team some recognition...</h1>
-      <button onClick={() => setShowModal(true)}>Create a new shout out</button>
+      <button onClick={displayShowModal}>Create a new shout out</button>
       {showModal ? <Modal onCancelClick={falseShowModal} /> : null}
+
+      {/*Here we create a div which holds a list of shoutouts*/}
+      {/*Why not add some styling to the shoutOutContainer, you could make it a grid?*/}
+      <div className="shoutOutContainer">
+          
+      </div>  
     </div>
   );
 };
 
 export default ShoutOuts;
+
